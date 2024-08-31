4e277e154a25b763efbe6cdc0be8b157ec25d330
iamvarunvj <vijay.mamidi@resmed.com.au>
2024-08-31 14:27:46 +0530

    arrays

--- 1.shellscripting/6.arrays.sh
+++ 1.shellscripting/6.arrays.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #index starts from 0, size is 3
-FRUITS=("APPLE","KIWI","ORANGE")  #Array
+FRUITS=("APPLE" "KIWI" "ORANGE")  #Array
 
 echo "First Fruit is: ${FRUITS[0]}"
 echo "Seconed Fruit is: ${FRUITS[1]}"
