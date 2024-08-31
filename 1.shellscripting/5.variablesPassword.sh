ebaea2fede676d1e1c3ebc182dcfe3b779ffa173
iamvarunvj <vijay.mamidi@resmed.com.au>
2024-08-31 14:01:47 +0530

    making password secret

--- 1.shellscripting/5.variablesPassword.sh
+++ 1.shellscripting/5.variablesPassword.sh
@@ -5,3 +5,9 @@ echo "Please enter your username::"
 read USERNAME #takesh the input from the user and stores into the variable USERNAME
 
 echo "Username entered is: $USERNAME"
+
+echo "Please enter your password::"
+
+read -s PWD #takesh the input from the user and stores into the variable USERNAME
+
+echo "Password entered is: $PWD"
