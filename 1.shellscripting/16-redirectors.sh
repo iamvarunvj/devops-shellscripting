0a0d55f5881cb97aa8b40ed497fd1b5dcf6581bf
iamvarunvj <vijay.mamidi@resmed.com.au>
2024-09-01 16:57:46 +0530

    Updated

--- 1.shellscripting/16-redirectors.sh
+++ 1.shellscripting/16-redirectors.sh
@@ -5,6 +5,7 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
 mkdir -p LOGS_FOLDER
+
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
