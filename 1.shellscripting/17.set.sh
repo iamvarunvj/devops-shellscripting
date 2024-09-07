ac4a850e780074d6837d0bbfd0be52d9d4405260
iamvarunvj <vijay.mamidi@resmed.com.au>
2024-09-07 15:25:14 +0530

    Updated

--- 1.shellscripting/17.set.sh
+++ 1.shellscripting/17.set.sh
@@ -4,7 +4,7 @@ set -e
 # setting the automatic exit if we get error. set -ex for debugging purpose.
 
 failure(){
-
+    echo "Failed at: $1:$2"
 }
 
 trap 'failure "${LINENO}" "${BASH_COMMAND}"' ERR # ERR is the error signal
