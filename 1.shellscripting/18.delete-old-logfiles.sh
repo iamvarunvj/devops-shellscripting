175fb5b252a68c3da9e5d7e0064f35f77211117b
iamvarunvj <vijay.mamidi@resmed.com.au>
2024-09-07 17:54:03 +0530

    Updated

--- 1.shellscripting/18.delete-old-logfiles.sh
+++ 1.shellscripting/18.delete-old-logfiles.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-which directory
-is that directory exists or not?
-only need .log files
+#which directory
+#is that directory exists or not?
+#only need .log files
 
 SOURCE_DIR=/home/ec2-user/logs
 R="\e[31m"
@@ -15,4 +15,9 @@ then
     echo -e "$SOURCE_DIR $G Source directory exsits $N"
 else
     echo -e "$SOURCE_DIR $R Source directory doesn't exsits $N"
+    exit 1
 fi
+
+FILES=$(find /home/ec2-user/logs -name "*.log")
+
+echo "Files $FILES"
